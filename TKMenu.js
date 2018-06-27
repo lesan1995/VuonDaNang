a53f9458aca45da2343cdcd11fe173b7bc30d115
lesan1995 <lesan1995@gmail.com>
2018-06-27 08:47:06 +0700

    abc

--- TKMenu.js
+++ TKMenu.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("#content").load("index.html");
+	$("#content").load("https://raw.githubusercontent.com/lesan1995/VuonDaNang/master/index.html");
 	$("input").on({
 		focus: function(){
 			$(this).css("background-color","#cccccc");
@@ -8,5 +8,8 @@ $(document).ready(function(){
 			$(this).css("background-color","#ffffff");
 		}
 
+	});
+	$("#dangKyVuon").click(function(){
+		$("#content").load("https://github.com/lesan1995/VuonDaNang/blob/master/DangKyVuon.html");
 	});
 });
